35b870a1e6f97f66238e103bb3741afda51ddedd
chnyang <chn.yang@qq.com>
2019-01-29 10:44:23 +0800

    第十章, 结构作为参数时,使用指针变量传递

--- ch10/main.c
+++ ch10/main.c
@@ -1,10 +1,13 @@
 #include "../inc/func.h"
 #include "../inc/struct.h"
 
-void about_student(Student const student) {
-    printf("我叫: %s\n", student.name);
-    printf("我读的学校是: %s\n", student.school.school_name);
+// C语言函数传值引用需要复制参数, 把结构作为函数参数时,
+// 使用指针比结构本身的拷贝效率能提高很多
+void about_student(Student *student) {
+    printf("我叫: %s\n", student->name);
+    printf("我读的学校是: %s\n", student->school.school_name);
 }
+
 int main(int argc, char const *argv[]) {
     School school = {
         .school_name = "人大附中",
@@ -13,9 +16,10 @@ int main(int argc, char const *argv[]) {
         .name = "雷婷婷",
         .school = school,
     };
+    printf("%s\n", student.name);
+    printf("%s\n", student.school.school_name);
+
     Student *st = &student;
-    printf("%s\n", st->name);
-    printf("%s\n", st->school.school_name);
-    about_student(student);
+    about_student(&student);
     return 0;
 }
